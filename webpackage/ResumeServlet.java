6224cd8b89ac309668c637d8b82ad0ebdaa2d74a
alekcguitar <alekcguitar@gmail.com>
2016-11-03 00:29:46 +0300

    add servlet

---
+++ webpackage/ResumeServlet.java
@@ -0,0 +1,26 @@
+package ru.webapp.webpackage;
+
+import java.io.IOException;
+import java.io.Writer;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Леха
+ * 02.11.2016.
+ */
+public class ResumeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        Writer writer = response.getWriter();
+        writer.write("тест сервлет: ну как оно, получается?)");
+        writer.close();
+    }
+}
